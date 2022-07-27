ab16c1741c3b24eccdea4fe7ea733cd75946f749
Duane Cilliers <hello@duane.co.za>
2022-07-27 21:19:26 +0200

    variable exercise

--- src/lectures/exercise/variables/variables.go
+++ src/lectures/exercise/variables/variables.go
@@ -25,4 +25,35 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Duane"
+	fmt.Println("My name is", myName)
+
+	var name string = "Kathy"
+	fmt.Println("My name = ", name)
+
+	userName := "admin"
+	fmt.Println("User name = ", userName)
+
+	var sum int
+	fmt.Println("Sum = ", sum)
+
+	part1, other := 1, 5
+	fmt.Println("Part = ", part1, "Other = ", other)
+
+	part2, other := 2, 0
+	fmt.Println("Part = ", part2, "Other = ", other)
+
+	sum = part1 + part2
+	fmt.Println("Sum = ", sum)
+
+	var(
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+	
+	fmt.Println("lessonName=", lessonName)
+	fmt.Println("lessonType=", lessonType)
+
+	word1, word2, _ := "Hello", "World", "!"
+	fmt.Println(word1, word2)
 }
