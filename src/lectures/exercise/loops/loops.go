6c59da12deafae1c663b2d0bbd8547a748133c5b
Duane Cilliers <hello@duane.co.za>
2022-07-29 20:06:01 +0200

    loops exercise

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -15,4 +15,13 @@ package main
 import "fmt"
 
 func main() {
+	for i := 1; i <= 50; i++ {
+		if i % 3 == 0 && i % 5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		}
+	}
 }
