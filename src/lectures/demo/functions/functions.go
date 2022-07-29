58d9f9ca5aed8b7ee6e17b14c5944e28db85241e
Duane Cilliers <hello@duane.co.za>
2022-07-29 19:18:20 +0200

    complete functions demo

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
+func double(x int) int {
+	return x + x
+}
 
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func great() {
+	fmt.Println("Hello from great function!")
+}
+
+func main() {
+	great()
+
+	dozen:= double(6)
+	fmt.Println(dozen)
+
+	bakersDozen := add(dozen, 1)
+	fmt.Println("A baker's dozen is", bakersDozen)
+
+	anotherBakersDozen := add(double(6), 1)
+	fmt.Println("Another baker's dozen is", anotherBakersDozen)
 }
