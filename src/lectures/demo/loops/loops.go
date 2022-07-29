17216abaff83a06fe55a78e45640d0887a28e1bc
Duane Cilliers <hello@duane.co.za>
2022-07-29 20:03:09 +0200

    loops demo

--- src/lectures/demo/loops/loops.go
+++ src/lectures/demo/loops/loops.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	sum := 0
+	fmt.Println("Sum:", sum)
 
+	for i := 1; i <= 10; i++ {
+		sum += i
+		fmt.Println("Sum:", sum)
+	}
+
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("Decrement. Sum is", sum)
+	}
 }
