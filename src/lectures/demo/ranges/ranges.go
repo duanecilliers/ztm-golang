cd20880ec59805257409b49121fcf2a18072a27d
Duane Cilliers <hello@duane.co.za>
2022-07-30 09:33:33 +0200

    ranges demo

--- src/lectures/demo/ranges/ranges.go
+++ src/lectures/demo/ranges/ranges.go
@@ -3,4 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	slice := []string{"Hello", "world", "!"}
+
+	for i, element := range slice {
+		fmt.Println(i, element, ":")
+		for _, ch := range element {
+			fmt.Printf("        %q\n", ch)
+		}
+	}
 }
