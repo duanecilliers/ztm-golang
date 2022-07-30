3bf186ae5086cf6788ff9cc1f93845e2e2537804
Duane Cilliers <hello@duane.co.za>
2022-07-30 10:17:01 +0200

    maps demo

--- src/lectures/demo/maps/maps.go
+++ src/lectures/demo/maps/maps.go
@@ -3,4 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 1
+	shoppingList["bread"] += 1
+
+	shoppingList["eggs"] += 1
+
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println("Milk deleted, new list:", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+	fmt.Println("cereal, found:", cereal, found)
+
+	totalItems := 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+	fmt.Println("total items:", totalItems)
 }
